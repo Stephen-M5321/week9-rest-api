29d58c6d6ed204ffdfefd7bf89dd13ee7898bfba
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-25 00:05:25 +0100

    user cruds added

---
+++ src/users/routes.js
@@ -0,0 +1,21 @@
+const { Router } = require("express")
+
+const userRouter = Router()
+
+const {registerUser,getAllUsers, updateUser, deleteUser, login } = require("./controllers") 
+const { hashPass, comparePass } = require("../middleware")
+
+userRouter.post("/users/register", hashPass, registerUser)
+
+userRouter.post("/users/login", comparePass, login)
+
+userRouter.get("/users/getUsers", getAllUsers)
+
+userRouter.put("/users/updateUser", updateUser)
+
+userRouter.delete("/users/deleteUser", deleteUser)
+
+
+//TODO: add rest of routes for each controller
+
+module.exports = userRouter
